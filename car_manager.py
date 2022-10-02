1b8bfeee671e01860959f32250d6b5af6b436ff5
Basith P <basithpofficial@gmail.com>
2022-10-02 16:17:40 +0530

    Create turtle and cars

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,29 @@
-COLORS = ["red", "orange", "green", "yello", "blue", "purple"]
+import random
+from turtle import Turtle
+
+
+COLORS = ["red", "orange", "green", "yellow", "blue", "purple"]
 INITIAL_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.cars = []
+
+    def create_car(self):
+        random_num = random.randint(1, 6)
+        if random_num != 1:
+            return
+        new_car = Turtle("square")
+        new_car.shapesize(stretch_len=2, stretch_wid=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y = random.randint(-250, 250)
+        new_car.goto(320, random_y)
+        self.cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.cars:
+            car.backward(INITIAL_MOVE_DISTANCE)
--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import time
 from turtle import Screen
+from car_manager import CarManager
+
+from player import Player
 
 
 screen = Screen()
+screen.title("Turtle Crossing")
 screen.setup(600, 600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+
 game_is_on = True
+
+screen.listen()
+screen.onkeypress(player.move, "Up")
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
 
-
-# screen.exitonclick()
+    car_manager.create_car()
+    car_manager.move_cars()
